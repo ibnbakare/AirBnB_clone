18298a94bc542b1371127a576215dff312fae575
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-11-25 13:15:01 +0100

    Task 2

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,26 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         if len(kwargs) != 0:
+            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.now())
+                if key == 'updated_at' or key == 'created_at':
+                    value = datetime.fromisoformat(value)
                 if key != '__class__':
-                	setattr(self, key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-    
+
     def save(self):
         self.updated_at = datetime.now()
-        
+
     def to_dict(self):
         new_dict = self.__dict__
         new_dict['__class__'] = self.__class__.__name__
