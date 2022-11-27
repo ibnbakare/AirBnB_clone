23cb19e144b586150db9d269a2c2cd766232b894
ridhwan <ridmartrite@gmail.com>
2022-11-27 18:23:58 +0100

    adding files again

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,10 @@ class BaseModel:
                     setattr(self, key, datetime.now())
                 if key != '__class__':
                 	setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            else:
+                self.id = str(uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
     
     def __str__(self):
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
